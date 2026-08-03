97729daba409be0061aa69d96a7faae0a120addd
Leonardo Loss <132675221+LeeoLoss@users.noreply.github.com>
2026-08-03 12:56:07 -0300

    feat: ajusta entidade Categoria e adiciona testes

--- src/FIAP.CloudGames.Domain/Catalog/Entities/Categoria.cs
+++ src/FIAP.CloudGames.Domain/Catalog/Entities/Categoria.cs
@@ -2,7 +2,42 @@ namespace FIAP.CloudGames.Domain.Catalog.Entities;
 
 public class Categoria
 {
-    public Guid Id { get; set; }
+    // 1 - Informando parämetros de construtor para o EF Core
+    public Guid Id { get; private set; }
 
-    public string Nome { get; set; } = string.Empty;
+    public string Nome { get; private set; } = string.Empty;
+
+    // 2 - Construtor privado para o EF Core
+    private Categoria()
+    {
+        
+    }
+
+    // 3 - Construtor público para criar uma nova categoria
+    public Categoria(Guid id, string nome)
+    {
+       if (id == Guid.Empty)
+            throw new ArgumentException("O identificador da categoria não pode ser vazio.", nameof(id));
+
+        Id = id;
+        AlterarNome(nome);
+    }
+
+    // 4. Método para alterar o nome da categoria
+    public void AlterarNome(string nome)
+    {
+        // Validação do nome da categoria
+        ArgumentException.ThrowIfNullOrWhiteSpace(nome, nameof(nome));
+
+        // Removendo espaços em branco no início e no final do nome
+        var nomeFormatado = nome.Trim();
+
+        // Validação do tamanho do nome da categoria
+        if (nomeFormatado.Length > 200)
+            throw new ArgumentException(
+                $"O nome da categoria não pode ter mais de 200 caracteres. Valor informado: '{nomeFormatado}'", nameof(nome));
+
+        Nome = nomeFormatado;    
+    }
+
 }
